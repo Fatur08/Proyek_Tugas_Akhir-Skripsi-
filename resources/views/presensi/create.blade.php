c0687f721c83be6f2f228c2a47b1e28e5b32dd0e
Muhammad Duta Faturrahman <dhutafaturrahman@gmail.com>
2024-06-23 22:07:02 +0700

    update

--- resources/views/presensi/create.blade.php
+++ resources/views/presensi/create.blade.php
@@ -20,7 +20,12 @@
         height: auto !important;
         border-radius: 15px;
     }
+    #map { 
+        height: 200px; 
+    }
  </style>
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"/>
+<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
 @endsection
 @section('content')
 <!-- App Capsule -->
@@ -32,10 +37,22 @@
 </div>
 <div class="row">
     <div class="col">
+        @if ($cek > 0)
+        <button id="takeabsen" class="btn btn-danger btn-block">
+            <ion-icon name="camera-outline"></ion-icon>    
+            Absen Pulang
+        </button>
+        @else
         <button id="takeabsen" class="btn btn-primary btn-block">
             <ion-icon name="camera-outline"></ion-icon>    
             Absen Masuk
         </button>
+        @endif
+    </div>
+</div>
+<div class="row mt-2">
+    <div class="col">
+        <div id="map"></div>
     </div>
 </div>
 @endsection
@@ -58,10 +75,58 @@
 
     function successCallback(position) {
         lokasi.value = position.coords.latitude + "," + position.coords.longitude;
+        var map = L.map('map').setView([position.coords.latitude, position.coords.longitude], 18);
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+        var marker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(map);
+        var circle = L.circle([position.coords.latitude, position.coords.longitude], {
+            color: 'red',
+            fillColor: '#f03',
+            fillOpacity: 0.5,
+            radius: 20
+        }).addTo(map);
     }
 
     function errorCallback() {
         s
     }
+
+    $("#takeabsen").click(function(e) {
+        Webcam.snap(function(uri){
+            image = uri;
+        });
+
+        var lokasi = $('#lokasi').val();
+        $.ajax({
+            type:'POST',
+            url:'/presensi/store',
+            data:{
+                _token:"{{ csrf_token() }}",
+                image:image,
+                lokasi:lokasi
+            },
+            cache:false,
+            success:function(respond){
+                var status = respond.split("|");
+                if(status[0] == "success"){
+                    Swal.fire({
+                      title: 'Berhasil !',
+                      text: status[1],
+                      icon: 'success'
+                    })
+                    setTimeout("location.href='/dashboard'", 3000);
+                } else {
+                    Swal.fire({
+                      title: 'Error !',
+                      text: 'Maaf gagal absen, silahkan hubungi petugas IT Sekolah',
+                      icon: 'error'
+                    })
+                    setTimeout("location.href='/dashboard'", 3000);
+                }
+            }
+        });
+    });
 </script>
 @endpush
