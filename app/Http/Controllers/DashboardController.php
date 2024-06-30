6bc35c53bda55324fb87d52ef63efca55520db91
Muhammad Duta Faturrahman <dhutafaturrahman@gmail.com>
2024-06-30 21:55:58 +0700

    Update 30 Juni 2024

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -11,13 +11,15 @@ class DashboardController extends Controller
     public function index()
     {
         $hariini = date("Y-m-d");
-        $bulanini = date("m"); // 1 atau Januari
+        $bulanini = date("m") * 1; // 1 atau Januari
         $tahunini = date("Y"); // 2024
         $nik = Auth::guard('karyawan')->user()->nik;
         $presensihariini = DB::table('presensi')->where('nik', $nik)->where('tgl_presensi',$hariini)->first();
         $historibulanini = DB::table('presensi')->whereRaw('MONTH(tgl_presensi)="'.$bulanini.'"')
         ->whereRaw('YEAR(tgl_presensi)="'. $tahunini . '"')
         ->orderBy('tgl_presensi')->get();
-        return view('dashboard.dashboard', compact('presensihariini','historibulanini'));
+
+        $namabulan = ["","Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","November","Desember"];
+        return view('dashboard.dashboard', compact('presensihariini','historibulanini','namabulan','bulanini','tahunini'));
     }
 }
